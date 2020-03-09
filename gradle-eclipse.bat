8fa6818f4bc74338b23611fd96976215adad4eab
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-09 23:56:31 +0900

    gradle sample

---
+++ gradle-eclipse.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set PJT_HOME=%~dp0\..
+set JAVA_HOME=%PJT_HOME%\tools\java\11
+set GRADLE_HOME=%PJT_HOME%\tools\gradle
+
+set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%GRADLE_HOME%\bin;%PATH%
+
+call cd %~dp0
+call gradle eclipse
+
+pause
